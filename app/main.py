d64f09f9e2346b967557e4f1da53a534bfc3eb8d
ELVERRUEDA <elver.rueda@cun.edu.co>
2025-04-08 12:06:42 -0500

    modificacion main para conectar con frontend

--- app/main.py
+++ app/main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 from app import routes
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
@@ -8,3 +9,11 @@ app.include_router(routes.router)
 @app.get("/")
 def home():
     return {"mensaje": "Bienvenido a la API de tareas con FastAPI y Firebase"}
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Puedes restringirlo a ["http://localhost:3000"]
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
